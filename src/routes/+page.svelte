4ed1e1e24c62c0c408a19891334e62a216e613eb
KevenMarioN <kevenmario.n.r@gmail.com>
2026-04-18 20:57:22 -0300

    feat(basic): learning how function same runes in svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,34 @@
 <script>
-    let name = "Keven";
+    // Use $state when you need a reactive variable
+    let count = $state(0);
+
+    // Use $derived when you need a new result derived from a $state
+    let doubleCount = $derived(count * 2);
+
+    // Use $effect when you need to execute another function when the state changes
+    $effect(() => {
+        console.log(`Count: ${count}, Double Count: ${doubleCount}`);
+    });
+
+    function increment() {
+        count++;
+    }
+
+    function decrement() {
+        count--;
+    }
+
+    function reset() {
+        count = 0;
+    }
 </script>
 
-<h1>Hello, {name}</h1>
+<button onclick={increment}>Increment</button>
+<button onclick={decrement}>Decrement</button>
 
-<style>
-    h1 {
-        color: blue;
-    }
-</style>
+<button onclick={reset}>Reset</button>
+
+<p>Counter: {count}</p>
+<p>Double Counter: {doubleCount}</p>
+
+<style></style>
